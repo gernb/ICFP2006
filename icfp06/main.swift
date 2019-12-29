572fe91b0488182c617c5eb64ac17408681c956d
peter bohac <pbohac@apple.com>
2019-12-28 20:44:41 -0800

    Initial Commit

---
+++ icfp06/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  icfp06
+//
+//  Created by peter bohac on 12/28/19.
+//  Copyright © 2019 peter bohac. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
